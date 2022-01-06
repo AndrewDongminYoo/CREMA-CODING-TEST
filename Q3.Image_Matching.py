c0319061d6b753dfccf7634b1b2d658951ae48b2
ydm2790 <ydm2790@gmail.com>
2022-01-06 18:45:00 +0900

    image matching

--- Q3.Image_Matching.py
+++ Q3.Image_Matching.py
@@ -14,35 +14,29 @@ import collections
 #
 
 def bread_first_search(y, x, grid, visited):
-    # Define directions.
-    dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+    directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]  # 4 방향 정의
 
-    # bfs results. - saves indexes which value is 1.
     bfs_results = [[y, x]]
 
-    # deque, that will be using in BFS.
-    deq = collections.deque()
-    # first, append (y, x) in deq.
-    deq.append((y, x))
-    while deq:
-        target_y, target_x = deq.popleft()
-        for direction in dirs:
+    queue = collections.deque()
+    queue.append((y, x))
+    while queue:
+        target_y, target_x = queue.popleft()
+        for direction in directions:
             new_y, new_x = target_y + direction[0], target_x + direction[1]
             if 0 <= new_y < len(grid):
                 if 0 <= new_x < len(grid[0]):
                     if visited[new_y][new_x] == 0:
                         if grid[new_y][new_x] == 1:
-                            deq.append((new_y, new_x))
+                            queue.append((new_y, new_x))
                             visited[new_y][new_x] = 1
                             bfs_results.append((new_y, new_x))
     return bfs_results
 
 
 def count_matches(grid_a, grid_b):
-    # Initialize answer
     answer = 0
 
-    # Initialize 2 visited array, one or grid1, the other one for grid2.
     visited_grid1 = []
     for y in range(len(grid_a)):
         temp = [0] * len(grid_a[0])
@@ -53,15 +47,11 @@ def count_matches(grid_a, grid_b):
         temp = [0] * len(grid_b[0])
         visited_grid2.append(temp)
 
-    # These two array will be compared for each other.
-    # if this two is same, the image area will be exactly SAME.
-
-    # Check starts.
     for y in range(len(grid_a)):
         for x in range(len(grid_a[0])):
-            if grid_a[y][x] == 1:
+            if grid_a[y][x] == 1:  # 두 그리드의 칸이 차 있는지 확인
                 if grid_b[y][x] == 1:
-                    if visited_grid1[y][x] == 0:
+                    if visited_grid1[y][x] == 0:  # 방문한 칸인지 확인
                         if visited_grid2[y][x] == 0:
                             visited_grid1[y][x] = 1
                             visited_grid2[y][x] = 1
@@ -73,8 +63,7 @@ def count_matches(grid_a, grid_b):
                             if grid1_bfs_result == grid2_bfs_result:
                                 answer += 1
 
-                            # Have to be removed all.
-                            grid1_bfs_result.clear()
+                            grid1_bfs_result.clear()  # deque 메소드
                             grid2_bfs_result.clear()
 
     return answer
